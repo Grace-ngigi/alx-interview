6ebd87c5bc97ba7e43f21a71b3fc0f2a033a3595
Grace_ngigi <gracengigi859@gmail.com>
2024-01-15 14:15:46 -0800

    correct pycodestyle errors

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,6 +3,7 @@
 NUMBER_OF_BITS_PER_BLOCK = 8
 MAX_NUMBER_OF_ONES = 4
 
+
 def validUTF8(data):
     ''' Validate utf-8 encodin '''
     data = iter(data)
@@ -16,11 +17,10 @@ def validUTF8(data):
                 return False    # Missing or illegal trailing byte
     return True
 
-   
-    
+
 def count_leading_bits(byte):
     '''count the number of leading set bits in a byte'''
     for i in range(8):
         if byte >> (7 - i) == 0b11111111 >> (7 - i) & ~1:
             return i
-    return 8   
+    return 8
