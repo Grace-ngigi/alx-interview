4538a2f4478eddfdae52274b7c69e156660a6c78
Grace_ngigi <gracengigi859@gmail.com>
2024-01-18 09:45:56 -0800

    star wars correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,26 @@
 // prints all caracters of certain star war movie
 
 const request = require('request');
-const movie_id = process.argv[2];
+const movieId = process.argv[2];
 
 const params = {
-url :`https://swapi-api.alx-tools.com/api/films/${movie_id}`,
-method: 'GET'
+  url: `https://swapi-api.alx-tools.com/api/films/${movieId}`,
+  method: 'GET'
 };
 
 request(params, (error, response, body) => {
   if (!error) {
-    const caracters_list = JSON.parse(body).characters;
-    printCaracter(caracters_list);
+    const caractersList = JSON.parse(body).characters;
+    printCaracter(caractersList);
   }
 });
 
-function printCaracter(characters) {
-   for (let idx = 0; idx < characters.length; idx++) {
-      request(characters[idx], function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+function printCaracter (characters) {
+  for (let idx = 0; idx < characters.length; idx++) {
+    request(characters[idx], function (error, response, body) {
+      if (!error) {
+        console.log(JSON.parse(body).name);
+      }
+    });
   }
+}
