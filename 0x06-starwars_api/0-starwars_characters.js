6f61ecefa4f14d59ec08bb8f8afeefcd313a07e4
Grace_ngigi <gracengigi859@gmail.com>
2024-01-18 10:07:20 -0800

    final commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,26 +9,26 @@ const params = {
   method: 'GET'
 };
 
-request(params, async(error, response, body) => {
+request(params, async (error, response, body) => {
   if (!error) {
     const caractersList = JSON.parse(body).characters;
-    for(const caractersUrl of caractersList){
-        const caracter = await printCaracter(caractersUrl);
-        console.log(caracter.name);
+    for (const caractersUrl of caractersList) {
+      const caracter = await printCaracter(caractersUrl);
+      console.log(caracter.name);
     }
   }
 });
 
 function printCaracter (caractersUrl) {
-    return new Promise((resolve, reject) => {
-        request(caractersUrl, function(error, response, body) {
-          if (error || response.statusCode !== 200) {
-            console.error('Error fetching character details:', error);
-            reject(error || response.statusCode);
-          } else {
-            const characterData = JSON.parse(body);
-            resolve(characterData);
-          }
-        });
-      });
+  return new Promise((resolve, reject) => {
+    request(caractersUrl, function (error, response, body) {
+      if (error || response.statusCode !== 200) {
+        console.error('Error fetching character details:', error);
+        reject(error || response.statusCode);
+      } else {
+        const characterData = JSON.parse(body);
+        resolve(characterData);
+      }
+    });
+  });
 }
